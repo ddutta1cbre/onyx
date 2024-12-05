47d56c5f01829ac196f939fb65970c44c243500a
pablodanswer <pablo@danswer.ai>
2024-12-05 15:11:25 -0800

    nits

--- web/tests/e2e/admin_pages.spec.ts
+++ web/tests/e2e/admin_pages.spec.ts
@@ -1,6 +1,38 @@
 import { test, expect } from "@playwright/test";
-import { verifyAdminPageNavigation } from "./utils/admin_setup";
 import testCases from "./testcases.json";
+import type { Page } from "@playwright/test";
+
+async function verifyAdminPageNavigation(
+  page: Page,
+  path: string,
+  pageTitle: string,
+  options?: {
+    paragraphText?: string | RegExp;
+    buttonName?: string;
+    subHeaderText?: string;
+  }
+) {
+  await page.goto(`http://localhost:3000/admin/${path}`);
+  await expect(page.locator("h1.text-3xl")).toHaveText(pageTitle);
+
+  if (options?.paragraphText) {
+    await expect(page.locator("p.text-sm").nth(0)).toHaveText(
+      options.paragraphText
+    );
+  }
+
+  if (options?.buttonName) {
+    await expect(
+      page.getByRole("button", { name: options.buttonName })
+    ).toHaveCount(1);
+  }
+
+  if (options?.subHeaderText) {
+    await expect(page.locator("h1.text-lg").nth(0)).toHaveText(
+      options.subHeaderText
+    );
+  }
+}
 
 for (const testCase of testCases) {
   test(
